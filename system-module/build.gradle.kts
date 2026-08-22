ff13259544d9cf096541b0a5fd7df927a228e130
霜冷长河 <11042077+silverpoetry@users.noreply.github.com>
2026-08-23 02:02:57 +0800

    chore: prepare 2.4.1 release

--- system-module/build.gradle.kts
+++ system-module/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.hyperears"
         minSdk = 35
         targetSdk = 36
-        versionCode = 20400
-        versionName = "2.4.0"
+        versionCode = 20401
+        versionName = "2.4.1"
     }
 
     signingConfigs {
